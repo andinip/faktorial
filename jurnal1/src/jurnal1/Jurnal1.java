91e7e3019452dfb4acaed5f57be77e06d66e8cf8
Andini Putri <andiniptr03@gmail.com>
2016-03-30 11:17:46 +0700

    Added files via upload

---
+++ jurnal1/src/jurnal1/Jurnal1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jurnal1;
+
+
+public class Jurnal1 {
+
+    int input; 
+    
+    public static void main(String[] args) {
+        System.out.println("Input : " +input);
+        if (input ){
+            for(){
+                System.out.println("random pokonya");
+            }
+        }
+    }
+    
+}
